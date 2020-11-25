064b9d08c82320f8ae1e19e257692bf2f7d52af0
Lalo-Mtz <gerardocct@gmail.com>
2020-11-24 21:33:20 -0600

    Primera estructura para el proyecto

--- index.php
+++ index.php
@@ -3,9 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ventas</title>
+    <title>R&L Store</title>
 </head>
 <body>
-    
+    <main>
+        <form action="inicia-sesion">
+            <label for="username">Username</label>
+            <input type="text" name="username" id="username">
+            <label for="paswd">Password</label>
+            <input type="text" name="paswd" id="paswd">
+        </form>
+    </main>
 </body>
 </html>
